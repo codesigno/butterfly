7a284f5c664fe393028c3d71dd6bc5a9503ac26f
Natalia <onataliadelina@gmail.com>
2022-01-18 18:51:32 +0100

    add 3d model

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FigurMonarch />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FigurMonarch from "./components/FigurMonarch.vue";
+import "./assets/css/main.css";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FigurMonarch,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/FigurMonarch.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="model">
+    <model-gltf
+      src="static/butterfly/butterfly.gltf"
+      :backgroundAlpha="bgAlpha"
+      :backgroundColor="bgColor"
+    >
+    </model-gltf>
+    <div class="txt">
+      <h1>beautiful butterfly</h1>
+      <h2>3D</h2>
+      <p>use mouse to rotate the butterfly</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ModelGltf } from "vue-3d-model";
+
+export default {
+  data() {
+    return {
+      bgColor: "#ff0",
+      bgAlpha: 0.1,
+    };
+  },
+  components: {
+    ModelGltf,
+  },
+};
+</script>
+
+<style>
+canvas {
+  width: 95vw;
+  height: 80vh;
+  background-image: url("../assets/blooming-field.jpg");
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+.model {
+  width: 90vw;
+  height: 95vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* flex-wrap: wrap; */
+  position: absolute;
+  top: 0;
+  z-index: 9;
+}
+.txt {
+  width: 18vw;
+  padding: 0 1rem;
+}
+h2 {
+  padding: 0.5rem 0;
+}
+.txt p {
+  text-align: center;
+}
+</style>
