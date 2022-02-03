0f07cc9ef8c6ccbdced191fdfe6f91776199c09b
Natalia <onataliadelina@gmail.com>
2022-02-03 14:33:02 +0100

    improve

--- src/components/ButterflyMonarch.vue
+++ src/components/ButterflyMonarch.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="model">
+    <div class="txt">
+      <h2>3D</h2>
+      <h1>Beautiful Butterfly</h1>
+      <p>use mouse or finger to resize or rotate 360°</p>
+      <img
+        src="../assets/img/rotate.png"
+        alt="rotate icon"
+        width="30"
+        id="rotate"
+      />
+    </div>
     <model-gltf
       src="static/butterfly/butterfly.gltf"
       :backgroundAlpha="bgAlpha"
       :backgroundColor="bgColor"
     >
     </model-gltf>
-    <div class="txt">
-      <h1>beautiful butterfly</h1>
-      <h2>3D</h2>
-      <p>use mouse to rotate the butterfly</p>
-    </div>
   </div>
 </template>
 
@@ -20,7 +26,7 @@ import { ModelGltf } from "vue-3d-model";
 export default {
   data() {
     return {
-      bgColor: "#ff0",
+      bgColor: "#e7deae",
       bgAlpha: 0.1,
     };
   },
@@ -32,31 +38,66 @@ export default {
 
 <style>
 canvas {
-  width: 95vw;
+  width: 99vw;
   height: 80vh;
   background-image: url("../assets/img/blooming-field.jpg");
-  background-size: contain;
+  background-size: cover;
   background-repeat: no-repeat;
 }
 .model {
-  width: 90vw;
+  width: 99vw;
   height: 95vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
-  /* flex-wrap: wrap; */
-  position: absolute;
-  top: 0;
-  z-index: 9;
 }
 .txt {
-  width: 18vw;
-  padding: 0 1rem;
+  width: 99vw;
+  padding: 0.3rem 1rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  text-align: center;
+  background-color: rgba(245, 240, 250, 0.692);
+  position: relative;
+  top: 1rem;
+  z-index: 1000;
+}
+h1 {
+  font-size: 1.3em;
 }
 h2 {
-  padding: 0.5rem 0;
+  padding-right: 0.5rem;
 }
 .txt p {
-  text-align: center;
+  margin-left: auto;
+  font-size: 0.8em;
+}
+
+#rotate {
+  animation: rotate 25s linear 0s infinite;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@media only screen and (max-width: 550px) {
+  .model {
+    width: 100vw;
+  }
+  canvas {
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+  }
+  .txt h1 {
+    font-size: 1.2em;
+  }
 }
 </style>
